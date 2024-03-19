c8f504887c69e8260dba5b0593b8ae32e164708f
ypjun100 <ypjun100@gmail.com>
2024-03-19 14:14:34 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/team13/n1/N1Application.java
@@ -0,0 +1,13 @@
+package com.team13.n1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N1Application.class, args);
+    }
+
+}
