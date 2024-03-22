dddc7482ac483572742d423fe2dc403bce9e0822
ypjun100 <ypjun100@gmail.com>
2024-03-22 16:34:15 +0900

    fix: 서버에서 정상적으로 DB connection이 이루어지지 않던 문제 해결

--- src/main/java/com/team13/n1/entity/User.java
+++ src/main/java/com/team13/n1/entity/User.java
@@ -1,7 +1,6 @@
 package com.team13.n1.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
