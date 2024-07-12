374a7253354e390b4f634fa30e6086779c1ac53c
ypjun100 <ypjun100@gmail.com>
2024-07-12 20:00:27 +0900

    프로젝트 초기화

--- src/main/java/com/team13/n1/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team13.n1.repository;
-
-import com.team13.n1.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Integer> {
-}
--- src/main/java/com/team13/n1/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team13.n1.repository;
-
-import com.team13.n1.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> { }
