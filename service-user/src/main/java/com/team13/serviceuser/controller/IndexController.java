2079e543c7043ee0f47ac63196dc9dc80a11c3f1
ypjun100 <ypjun100@gmail.com>
2024-07-15 16:44:22 +0900

    feat: 각 서비스의 IndexController 추가

---
+++ service-user/src/main/java/com/team13/serviceuser/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.team13.serviceuser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+    @GetMapping
+    public String index() {
+        return "Index page of service-user";
+    }
+}
