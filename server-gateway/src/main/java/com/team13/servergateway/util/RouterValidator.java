257ef30d4b60ba43dc3f63655c360d1929305970
ypjun100 <ypjun100@gmail.com>
2024-07-31 17:01:09 +0900

    feat: server-gateway 특정 URL에서만 로그인 토큰을 요구하게끔 수정 feat: gateway 라우팅 URL 수정

---
+++ server-gateway/src/main/java/com/team13/servergateway/util/RouterValidator.java
@@ -0,0 +1,23 @@
+package com.team13.servergateway.util;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RouterValidator {
+
+    // 로그인 토큰이 요구되는 path
+    private final List<List<String>> securedApiEndpoints = List.of(
+            List.of("POST", "/recipe")
+    );
+
+    // 만약 request의 path가 securedApiEndpoints 중 하나인 경우 true를 반환하고, 그렇지 않으면 false를 반환함
+    public boolean isSecured(ServerHttpRequest request) {
+        return securedApiEndpoints.stream().anyMatch(methodAndPath ->
+                request.getMethod().toString().equals(methodAndPath.get(0)) &&
+                request.getURI().getPath().contains(methodAndPath.get(1))
+        );
+    }
+}
