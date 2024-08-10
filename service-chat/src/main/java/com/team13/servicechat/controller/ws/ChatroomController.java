73270aad79e99632765daa48221cab73fb805ef6
ypjun100 <ypjun100@gmail.com>
2024-08-10 23:15:48 +0900

    feat: SUBSCRIBE 메시지 처리 feat: ENTER, EXIT_SESSION 메시지 제거 feat: 컨트롤러, 서비스 로직 분리

--- service-chat/src/main/java/com/team13/servicechat/controller/ws/ChatroomController.java
+++ service-chat/src/main/java/com/team13/servicechat/controller/ws/ChatroomController.java
@@ -4,7 +4,6 @@ import com.team13.servicechat.dto.ChatroomMessage;
 import com.team13.servicechat.entity.ChatroomMessages;
 import com.team13.servicechat.service.ChatroomMessagesService;
 import com.team13.servicechat.service.ws.ChatroomService;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -15,8 +14,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -25,85 +22,67 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ChatroomController {
 
-    // 접속되어 있는 유저의 정보를 담기위한 레코드
-    private record UserInfo(@NonNull String userId, @NonNull String userName) {}
-
     private final ChatroomService service;                    // 채팅방 웹 소켓 서비스
     private final ChatroomMessagesService messagesService;    // 채팅방 메시지 서비스
 
     private final SimpMessagingTemplate template;             // 소켓 메시지 전송을 위한 템플릿
 
-    // 접속되어 있는(구독을 완료한) 사용자들의 정보를 담고있는 해쉬맵
-    // Key: 세션 ID
-    // Value: 유저 정보
-    private final Map<String, UserInfo> validUserInfos = new HashMap<>();
-
     @SubscribeMapping("/chatroom/{chatroomId}")
     public void subscribeMessage(@DestinationVariable String chatroomId,
                                  SimpMessageHeaderAccessor accessor) {
+
         // 유저 ID나 이름을 불러올 수 없다면 해당 메시지를 처리하지 않음
         if (accessor.getNativeHeader("userId") == null ||
             accessor.getNativeHeader("userName") == null)
             return;
 
-        // 세션 ID와 유저 정보 저장
-        validUserInfos.put(accessor.getSessionId(), new UserInfo(
-                Objects.requireNonNull(accessor.getNativeHeader("userId")).get(0),
-                Objects.requireNonNull(accessor.getNativeHeader("userName")).get(0)
-        ));
+        String userId = Objects.requireNonNull(accessor.getNativeHeader("userId")).get(0);
+        String userName = Objects.requireNonNull(accessor.getNativeHeader("userName")).get(0);
+
+        service.subscribe(chatroomId, Long.valueOf(userId), userName, accessor.getSessionId());
     }
+
 
     // 전체 메시지 관리 핸들러
     @MessageMapping("/chatroom/{chatroomId}")  // 클라이언트 -> 서버 ('/ws/publish/chatroom/{chatroomId}')
     public void messageHandler(@DestinationVariable String chatroomId,
                         @Payload ChatroomMessage message,
                         SimpMessageHeaderAccessor accessor) {
+
         log.info("IN : " + message);
-
-        // 유효한 유저(구독을 완료한 유저)가 아닌 경우 해당 메시지를 처리하지 않음
-        if (!validUserInfos.containsKey(accessor.getSessionId())) {
-            return;
-        }
-
-        // 메시지에 유저 ID와 이름을 메시지에 저장
-        message.setSenderUserId(validUserInfos.get(accessor.getSessionId()).userId);
-        message.setSenderUserName(validUserInfos.get(accessor.getSessionId()).userName);
 
         // 채팅방 내의 사용자들에게 전달될 메시지
         Optional<ChatroomMessage> response = Optional.empty();
 
         // 메시지 타입에 따른 서비스 라우팅
-        if (message.getType() == ChatroomMessage.MessageType.ENTER) {
-            response = service.messageEnter(chatroomId, message);
-
-        } else if (message.getType() == ChatroomMessage.MessageType.MESSAGE_TEXT ||
-                   message.getType() == ChatroomMessage.MessageType.MESSAGE_IMAGE ) {
-            response = service.messageTextAndImage(chatroomId, message);
+        if (message.getType() == ChatroomMessage.MessageType.MESSAGE_TEXT ||
+            message.getType() == ChatroomMessage.MessageType.MESSAGE_IMAGE ) {
+            response = service.messageTextAndImage(chatroomId, message, accessor.getSessionId());
 
         } else if (message.getType() == ChatroomMessage.MessageType.EXIT_USER) {
-            response = service.messageExitUser(chatroomId, message);
+            response = service.messageExitUser(chatroomId, message, accessor.getSessionId());
 
         } else if (message.getType() == ChatroomMessage.MessageType.COMPLETE) {
-            response = service.messageComplete(chatroomId, message);
+            response = service.messageComplete(chatroomId, message, accessor.getSessionId());
 
         }
 
         // 전달할 메시지가 있는 경우에만 채팅방 내 사용자에게 메시지를 전달함
         if (response.isPresent()) {
-            log.info("OUT : " + response);
-
             // MySQL 서버에 메시지 저장
             ChatroomMessages savedMessage = messagesService.saveMessage(ChatroomMessages.builder()
                             .chatroomsId(chatroomId)
                             .type(response.get().getType().toString())
                             .message(response.get().getMessage())
-                            .senderUsersId(Long.parseLong(message.getSenderUserId()))
+                            .senderUsersId(response.get().getSenderUserId())
                             .build());
 
             // MongoDB 서버에 해당 메시지의 ID 저장
             service.addMessageIdInChatroom(chatroomId, savedMessage);
 
             template.convertAndSend("/ws/subscribe/chatroom/" + chatroomId, response);
+
+            log.info("OUT : " + response);
         }
     }
 }
--- service-chat/src/main/java/com/team13/servicechat/service/ws/ChatroomService.java
+++ service-chat/src/main/java/com/team13/servicechat/service/ws/ChatroomService.java
@@ -4,6 +4,8 @@ import com.team13.servicechat.dto.ChatroomMessage;
 import com.team13.servicechat.entity.ChatroomMessages;
 import com.team13.servicechat.entity.Chatrooms;
 import com.team13.servicechat.repository.ChatroomsRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -11,58 +13,65 @@ import java.util.*;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ChatroomService {
+
+    // 접속되어 있는 유저의 정보를 담기위한 레코드
+    private record User(@NonNull Long userId, @NonNull String userName, @NonNull String sessionId) { }
 
     // 채팅방 메시지 데이터를 저장하는 레포지토리
     private final ChatroomsRepository repository;
 
-    // 채팅방 목록 (repository와의 잦은 통신을 줄이기 위한 캐싱을 담당함)
-    private final HashSet<String> chatroomIds;
+    // 각 채팅방 내에 접속 중인 유저 정보를 저장하는 해쉬맵
+    private final Map<String, List<User>> chatrooms = new HashMap<>();
 
-    public ChatroomService(ChatroomsRepository repository) {
-        this.repository = repository;
 
-        chatroomIds = new HashSet<>();
+
+    // SUBSCRIBE 메시지 관리
+    public void subscribe(String chatroomId, Long userId, String userName, String sessionId) {
+        verify(chatroomId, userId, userName, sessionId);
+        log.info(chatrooms);
     }
 
-    // 채팅방 입장
-    public Optional<ChatroomMessage> messageEnter(String chatroomId, ChatroomMessage message) {
-        // 존재하는 채팅방인 경우 메시지 발신자를 해당 채팅방에 입장시킴
-        if (verifyChatroomId(chatroomId)) {
-            // DB에 해당 유저 정보 추가
-            Chatrooms chatroom = repository.findById(chatroomId).orElseThrow();
-
-            chatroom.addUserId(Long.parseLong(message.getSenderUserId()));
-            repository.save(chatroom);
-
-            return Optional.of(ChatroomMessage.builder()
-                            .type(ChatroomMessage.MessageType.NOTICE)
-                            .message(message.getSenderUserName() + "님이 입장하셨습니다.")
-                            .build());
-        }
-
-        return Optional.empty();
-    }
 
     // 채팅 메시지
-    public Optional<ChatroomMessage> messageTextAndImage(String chatroomId, ChatroomMessage message) {
-        // 채팅방이 존재하는 경우에만 메시지를 반환함(반환한다는 의미는 이 메시지를 DB에 저장한다는 의미임)
-        if (verifyChatroomId(chatroomId)) {
+    public Optional<ChatroomMessage> messageTextAndImage(String chatroomId,
+                                                         ChatroomMessage message,
+                                                         String sessionId) {
+
+        // 유효한 채팅방 ID, 유저인 경우에만 메시지를 반환함 (반환한다는 의미는 이 메시지를 DB에 저장한다는 의미임)
+        if (verify(chatroomId, message, sessionId)) {
+            // 유저 이름을 가져와 메시지에 저장
+            User sender = getUserInChatrooms(chatroomId, sessionId);
+            message.setSenderUserId(sender.userId);
+            message.setSenderUserName(sender.userName);
+
+            // TODO: feign을 통한 다른 서비스들에게도 채팅방 메시지 전달
+
             return Optional.of(message);
         }
 
         return Optional.empty();
     }
 
-    // 채팅방에서 유저 퇴장
-    public Optional<ChatroomMessage> messageExitUser(String chatroomId, ChatroomMessage message) {
-        // 나가려는 채팅방이 존재하는지 확인
-        if (verifyChatroomId(chatroomId)) {
+
+    // 유저의 공동구매 포기
+    public Optional<ChatroomMessage> messageExitUser(String chatroomId,
+                                                     ChatroomMessage message,
+                                                     String sessionId) {
+
+        // 유효한 채팅방 ID, 유저인 경우에만 메시지를 반환함 (반환한다는 의미는 이 메시지를 DB에 저장한다는 의미임)
+        if (verify(chatroomId, message, sessionId)) {
+            // 유저 이름을 가져와 메시지에 저장
+            User sender = getUserInChatrooms(chatroomId, sessionId);
+            message.setSenderUserId(sender.userId);
+            message.setSenderUserName(sender.userName);
+
             // 채팅방 데이터를 가져옴
             Chatrooms chatroom = repository.findById(chatroomId).orElseThrow();
 
             // 해당 채팅방에서 유저가 포함되어 있는지 확인
-            int userIndex = chatroom.getUsersIds().indexOf(Long.valueOf(message.getSenderUserId()));
+            int userIndex = chatroom.getUsersIds().indexOf(message.getSenderUserId());
 
             if (userIndex != -1) {
                 // 퇴장 유저 정보를 채팅방 내에서 제거
@@ -71,16 +80,18 @@ public class ChatroomService {
                 // 만약 모든 유저가 채팅방에서 나간 경우, 해당 채팅방 제거
                 if (chatroom.getUsersIds().isEmpty()) {
                     repository.deleteById(chatroomId);
-                    chatroomIds.remove(chatroomId);
+                    chatrooms.remove(chatroomId);
 
                     // TODO: feign을 통한 다른 서비스들에게도 채팅방 삭제 메시지 전달
+
+                    repository.deleteById(chatroomId);
 
                     return Optional.empty();
                 }
 
                 return Optional.of(ChatroomMessage.builder()
                                 .type(ChatroomMessage.MessageType.NOTICE)
-                                .message(message.getSenderUserName() + "님이 퇴장하셨습니다.")
+                                .message(message.getSenderUserName() + "님이 공동구매를 포기하셨습니다.")
                                 .build());
             }
         }
@@ -88,8 +99,11 @@ public class ChatroomService {
         return Optional.empty();
     }
 
+
     // TODO: 공동구매 완료
-    public Optional<ChatroomMessage> messageComplete(String chatroomId, ChatroomMessage message) {
+    public Optional<ChatroomMessage> messageComplete(String chatroomId,
+                                                     ChatroomMessage message,
+                                                     String sessionId) {
        /*
        공동구매 완료 기능은 공동구매 게시자만 요청할 수 있으며, 게시자가 완료 요청시 해당 메시지를 전체 클라이언트에게 전송합니다.
        해당 메시지를 받은 다른 사용자들은 리뷰를 작성하면 messageExitUser() 메서드를 통해 채팅방을 나갈 수 있으며, 모든 유저가
@@ -98,6 +112,10 @@ public class ChatroomService {
 
         return Optional.empty();
     }
+
+
+    // TODO: 공동구매 참여시 채팅방 내에 있는 유저들에게 '~님이 공동구매에 참여하였습니다.' 메시지 전달
+
 
     // 채팅방에 해당 메시지 아이디 추가
     public void addMessageIdInChatroom(String chatroomId, ChatroomMessages message) {
@@ -112,29 +130,65 @@ public class ChatroomService {
         }
     }
 
-    // 존재하는 채팅방인지 확인
-    // 가장 먼저, 캐쉬에 저장된 채팅방 ID인지 확인한 후 캐쉬에 없으면, DB를 확인하여 존재하는 아이디인지 확인
+
+    /*
+    verify~() 메서드는 올바른 채팅방인지 혹은 특정 채팅방에 권한이 있는 사용자인지를 판별합니다.
+    이 메서드들은 먼저 클래스 내부에 선언된 chatrooms 해쉬맵을 통해 유효한지 판별하게 되고, 만약 해쉬맵을
+    통해 확인하지 못한다면, repository를 통해 다시 한번 유효한지 판별하게 됩니다. 또한 repository의
+    정보가 chatrooms에 반영되지 않은 경우 이를 반영하여 최대한 repository에 접근하는 것을 방지하고
+    chatrooms를 캐쉬 용도로 사용할 수 있게끔 합니다.
+     */
+
+    // 존재하는 채팅방 ID인지 확인
     private boolean verifyChatroomId(String chatroomId) {
-        log.info(repository.findAll());
 
-        if (chatroomIds.contains(chatroomId)) {
-            // 해당 채팅방의 ID가 캐시 채팅방 목록에 저장되어 있는 경우
+        if (chatrooms.containsKey(chatroomId)) {
             return true;
-
         } else if (repository.existsById(chatroomId)) {
-            // 채팅방의 ID가 캐시 채팅방에는 없으나 DB에 있는 경우 해당 아이디를 캐쉬에 저장
-            chatroomIds.add(chatroomId);
+            chatrooms.put(chatroomId, new ArrayList<>());  // 유효한 채팅방인지 확인함과 동시에 chatrooms에 채팅방을 추가시킴
             return true;
         }
 
-        // 두 저장소 모두 해당 아이디가 없는 경우 false 반환
         return false;
     }
 
-    // TODO: 채팅방 내에 존재하는 유저인지 확인
-    // verifyChatroomId와 마찬가지로 캐시에서 유저 ID를 확인한 뒤, DB를 확인하여 유저 검증 진행
-    private boolean verifyUserIdInChatroom(String chatroomId, String userId) {
+
+    // 사용자가 전달한 chatroomId와 userId가 올바른 아이디인지 확인
+    // 만약 존재하지 않는 채팅방이거나, 해당 채팅방에 대한 권한이 없는 유저의 경우 false를 반환함
+    private boolean verify(String chatroomId, ChatroomMessage message, String sessionId) {
+
+        Long userId = message.getSenderUserId();
+        String userName = message.getSenderUserName();
+
+        return verify(chatroomId, userId, userName, sessionId);
+    }
+
+    private boolean verify(String chatroomId, Long userId, String userName, String sessionId) {
+
+        // 존재하는 채팅방 ID인지 확인
+        if (verifyChatroomId(chatroomId)) {
+            // chatrooms 내에 등록된 세션 ID인지 확인
+            if (chatrooms.get(chatroomId).stream().anyMatch((user) -> user.sessionId.equals(sessionId))) {
+                return true;
+            }
+
+            // repository 내에서 존재하는 유저 ID인지 확인
+            List<Long> users = repository.findById(chatroomId).orElseThrow().getUsersIds();
+            if (users.contains(userId)) {
+                chatrooms.get(chatroomId).add(new User(userId, userName, sessionId));
+                return true;
+            }
+        }
+
         return false;
     }
 
+
+    // chatrooms 내에서 특정 sessionId를 갖는 유저 객체 반환
+    private User getUserInChatrooms(String chatroomId, String sessionId) {
+        return chatrooms.get(chatroomId).stream()
+                .filter(user -> user.sessionId.equals(sessionId))
+                .findFirst()
+                .orElseThrow();
+    }
 }
