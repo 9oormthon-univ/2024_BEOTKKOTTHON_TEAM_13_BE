45fe7c54b60779a5fa037716acc711a0a6f9a776
joamksh <joamksh@naver.com>
2024-08-10 23:29:40 +0900

    Delete service-user/src/main/java/com/team13/serviceuser/repository/UserRepository.java
    user-entity외 기능 commit삭제

--- service-user/src/main/java/com/team13/serviceuser/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team13.serviceuser.repository;
-
-import com.team13.serviceuser.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
