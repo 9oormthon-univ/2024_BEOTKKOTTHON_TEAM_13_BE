e8c00e35bcd3b0d6509c584bce13f7e010c43ea0
ypjun100 <ypjun100@gmail.com>
2024-08-17 17:33:50 +0900

    feat: service-user의 JWT 페이로드 데이터 수정 feat: log4j2 설정

--- service-user/src/main/java/com/team13/serviceuser/service/SignInService.java
+++ service-user/src/main/java/com/team13/serviceuser/service/SignInService.java
@@ -1,5 +1,6 @@
 package com.team13.serviceuser.service;
 
+import com.team13.serviceuser.entity.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -12,7 +13,7 @@ import java.util.Map;
 
 @Service
 public class SignInService {
-    private final Key tokenKey; // JWT 토큰을 위한 키
+    private final Key jwtSecretKey; // JWT 토큰을 위한 암호키
 
     private final long tokenKeepDuration; // 토큰 유지 기간(밀리초)
 
@@ -21,29 +22,58 @@ public class SignInService {
 
         // 암호키를 바탕으로 키 생성
         byte[] keyBytes = Decoders.BASE64.decode(tokenSecret);
-        this.tokenKey = Keys.hmacShaKeyFor(keyBytes);
+        this.jwtSecretKey = Keys.hmacShaKeyFor(keyBytes);
 
         // 토큰 유지 기간
         this.tokenKeepDuration = tokenKeepDuration * 1000; // 초를 밀리초로 변환
     }
 
     // 유저 로그인 정보 검증
-    public boolean verifyLoginInfo(Map<String, String> loginInfo) {
-        if (loginInfo.containsKey("user_id") && loginInfo.containsKey("password")) {
-            // 유저 인증 메커니즘 추가
+    public boolean verifyLoginInfo(String userEmail, String userPassword) {
+
+        // TODO: 유저 인증 메커니즘 추가 (아래는 테스트용 코드)
+        if (userEmail.equals("ypjun100@gmail.com") ||
+                userEmail.equals("ypjun101@gmail.com")) {
             return true;
         }
+
         return false;
     }
 
+
+    // 유저 정보 반환
+    public User getUserByEmail(String userEmail) {
+
+        // TODO: 유저 이메일로 유저 정보 가져오는 코드 추가 (아래는 테스트용 코드)
+        if (userEmail.equals("ypjun100@gmail.com")) {
+
+            return User.builder()
+                    .id(1L)
+                    .nickname("ypjun100")
+                    .build();
+
+        } else if (userEmail.equals("ypjun101@gmail.com")) {
+
+            return User.builder()
+                    .id(2L)
+                    .nickname("ypjun101")
+                    .build();
+
+        }
+
+        return User.builder().build();
+    }
+
+
     // 토큰 생성
-    public String createToken(String userId) {
+    public String createTokenByUser(User user) {
         long now = (new Date()).getTime();
 
         return Jwts.builder()
-                .claim("userId", userId)
+                .claim("userId", user.getId())
+                .claim("userNickname", user.getNickname())
                 .setExpiration(new Date(now + tokenKeepDuration))
-                .signWith(tokenKey)
+                .signWith(jwtSecretKey)
                 .compact();
     }
 }
