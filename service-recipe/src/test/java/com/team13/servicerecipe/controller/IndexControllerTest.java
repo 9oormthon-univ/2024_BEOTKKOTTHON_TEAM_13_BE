210a260fa8c5034327d27c514b3c1209736c821d
ypjun100 <ypjun100@gmail.com>
2024-08-24 21:41:05 +0900

    fix: 테스트 코드 제거

--- service-recipe/src/test/java/com/team13/servicerecipe/controller/IndexControllerTest.java
+++ service-recipe/src/test/java/com/team13/servicerecipe/controller/IndexControllerTest.java
@@ -13,20 +13,20 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest(properties = "spring.profiles.active=dev")
+//@AutoConfigureMockMvc
+//@ExtendWith(MockitoExtension.class)
+//@SpringBootTest(properties = "spring.profiles.active=dev")
 class IndexControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @Test
-    public void IndexController_BriefList_Default() throws Exception {
-
-    }
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @Autowired
+//    private ObjectMapper objectMapper;
+//
+//    @Test
+//    public void IndexController_BriefList_Default() throws Exception {
+//
+//    }
 
 }
