d65500dbf4d0713047076356f96da3622766d9ca
joamksh <joamksh@naver.com>
2024-08-24 22:14:07 +0900

    ref : msa 신버전 반영

---
+++ service-user/src/main/java/com/team13/serviceuser/controller/IndexController.java
@@ -0,0 +1,105 @@
+package com.team13.serviceuser.controller;
+
+import com.team13.serviceuser.dto.JoinRequest;
+import com.team13.serviceuser.dto.ResponseDto;
+import com.team13.serviceuser.entity.User;
+import com.team13.serviceuser.service.SignInService;
+import com.team13.serviceuser.service.SignUpService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.ObjectError;
+import java.util.Map;
+
+@Log4j2
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class IndexController {
+
+    @Value("${app.test-string}")
+    private String configTestString;
+
+    private final SignInService signInService;
+
+    private final SignUpService signUpService;
+
+    @GetMapping
+    public String index() { return "Index page of service-user"; }
+
+    //회원가입 요청
+    // 회원가입 요청
+    @PostMapping("/join")
+        // loginId 중복 체크
+        public ResponseEntity<String> join (@Valid @RequestBody JoinRequest joinRequest, BindingResult bindingResult) {
+            if (bindingResult.hasErrors()) {
+                String errorMessages = bindingResult.getAllErrors()
+                        .stream()
+                        .map(ObjectError::getDefaultMessage)
+                        .reduce((message1, message2) -> message1 + "; " + message2)
+                        .orElse("Validation failed.");
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            if (signUpService.checkEmailDuplicate(joinRequest.getEmail())) {
+                return ResponseEntity.badRequest().body("이메일이 중복됩니다.");
+            }
+            // 닉네임 중복 체크
+
+            if (signUpService.checkNicknameDuplicate(joinRequest.getNickname())) {
+                return ResponseEntity.badRequest().body("닉네임이 중복됩니다.");
+            }
+            // password와 passwordCheck가 같은지 체크
+
+            if (!joinRequest.getPassword().equals(joinRequest.getPasswordCheck())) {
+                return ResponseEntity.badRequest().body("비밀번호가 일치하지 않습니다.");
+            }
+
+            signUpService.join(joinRequest);
+            return ResponseEntity.ok("회원가입 성공");
+        }
+
+
+    // 로그인 요청
+    // 로그인 성공 시 클라이언트에게 JWT 토큰을 반환함
+    @PostMapping("/signin")
+    public ResponseEntity<String> signIn(@RequestBody Map<String, String> loginInfo,
+                                         HttpServletResponse response) {
+
+        // 사용자가 email과 password를 모두 전송했는지 확인
+        if (loginInfo.containsKey("email") && loginInfo.containsKey("password")) {
+
+            // 사용자가 전송한 유저 데이터가 DB에 있는 데이터인지 확인
+            if (signInService.verifyLoginInfo(loginInfo.get("email"), loginInfo.get("password"))) {
+
+                // 유저 이메일로 유저 정보를 가져옴
+                User user = signInService.getUserByEmail(loginInfo.get("email"));
+
+                // JWT 쿠키 생성 및 response에 쿠키 추가
+                Cookie cookie = signInService.createCookieFromUser(user);
+                response.addCookie(cookie);
+
+                return ResponseEntity.ok().build();
+            }
+
+        }
+
+        // 존재하지 않는 계정인 경우, 클라이언트로 401 코드를 전송함
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/config")
+    public String getConfig() { return configTestString; }
+
+    @GetMapping("/service-connection-test")
+    public ResponseDto serviceConnectionTest() {
+        return new ResponseDto("Greeting!!");
+    }
+
+}
---
+++ service-user/src/main/java/com/team13/serviceuser/service/SignInService.java
@@ -0,0 +1,90 @@
+package com.team13.serviceuser.service;
+
+import com.team13.serviceuser.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class SignInService {
+    private final Key jwtSecretKey; // JWT 토큰을 위한 암호키
+
+    private final long tokenKeepDuration; // 토큰 유지 기간(밀리초)
+
+    public SignInService(@Value("${app.jwt.secret}") String tokenSecret,
+                         @Value("${app.jwt.keep}") long tokenKeepDuration) {
+
+        // 암호키를 바탕으로 키 생성
+        byte[] keyBytes = Decoders.BASE64.decode(tokenSecret);
+        this.jwtSecretKey = Keys.hmacShaKeyFor(keyBytes);
+
+        // 토큰 유지 기간
+        this.tokenKeepDuration = tokenKeepDuration * 1000; // 초를 밀리초로 변환
+    }
+
+  
+    // 유저 로그인 정보 검증
+    public boolean verifyLoginInfo(String userEmail, String userPassword) {
+
+        // TODO: 유저 인증 메커니즘 추가 (아래는 테스트용 코드)
+        if (userEmail.equals("ypjun100@gmail.com") ||
+                userEmail.equals("ypjun101@gmail.com")) {
+            return true;
+        }
+
+        return false;
+    }
+
+  
+    // 유저 정보 반환
+    public User getUserByEmail(String userEmail) {
+
+        // TODO: 유저 이메일로 유저 정보 가져오는 코드 추가 (아래는 테스트용 코드)
+        if (userEmail.equals("ypjun100@gmail.com")) {
+
+            return User.builder()
+                    .id(1L)
+                    .nickname("ypjun100")
+                    .build();
+
+        } else if (userEmail.equals("ypjun101@gmail.com")) {
+
+            return User.builder()
+                    .id(2L)
+                    .nickname("ypjun101")
+                    .build();
+
+        }
+
+        return User.builder().build();
+    }
+
+
+    // JWT 쿠키 생성
+    public Cookie createCookieFromUser(User user) {
+
+        // 쿠키 만료 시간 설정을 위한 현재 시간 데이터
+        long now = (new Date()).getTime();
+
+        // JWT 토큰 생성
+        String token = Jwts.builder()
+                            .claim("userId", user.getId().toString())  // ID는 개발 편의상 문자열로 저장
+                            .claim("userNickname", user.getNickname())
+                            .setExpiration(new Date(now + tokenKeepDuration))
+                            .signWith(jwtSecretKey)
+                            .compact();
+
+        // JWT 쿠키 생성
+        Cookie cookie = new Cookie("LTK", token);
+        cookie.setMaxAge((int) tokenKeepDuration / 1000);
+        cookie.setPath("/");
+
+        return cookie;
+    }
+}
