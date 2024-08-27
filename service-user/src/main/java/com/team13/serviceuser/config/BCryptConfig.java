0f107c7bb2492477202127f5219df5e19a8fb1b3
joamksh <joamksh@naver.com>
2024-08-27 12:55:31 +0900

    feat : 회원가입 로그인

---
+++ service-user/src/main/java/com/team13/serviceuser/config/BCryptConfig.java
@@ -0,0 +1,14 @@
+package com.team13.serviceuser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//비밀 번호 암호화
+@Configuration
+public class BCryptConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
