6e77f826d6dc6a40096bb21a420f44b177a30a01
joamksh <joamksh@naver.com>
2024-08-28 22:16:57 +0900

    feat : Ingrident Post Service에서 한 번에 확인

---
+++ service-post/src/main/java/com/team13/servicepost/dto/PostIngredientDto.java
@@ -0,0 +1,11 @@
+package com.team13.servicepost.dto;
+
+import lombok.Data;
+
+@Data
+public class PostIngredientDto {
+    private Long id;
+    private Long postId;
+    private String name;
+    private String url;
+}
