91c42da6752c73da8a91772a3f943aa074b8606d
joamksh <joamksh@naver.com>
2025-01-18 21:05:32 +0900

    ref : 토큰 불필요한 기능 필터 적용 수정

--- server-gateway/src/main/java/com/team13/servergateway/util/RouterValidator.java
+++ server-gateway/src/main/java/com/team13/servergateway/util/RouterValidator.java
@@ -18,25 +18,7 @@ public class RouterValidator {
     );
 
     // 만약 request의 path가 securedApiEndpoints 중 하나인 경우 true를 반환하고, 그렇지 않으면 false를 반환함
-    // 로그인 토큰이 필요하지 않은 path
-    private final List<List<String>> openApiEndpoints = List.of(
-            List.of("POST", "/sign/login"),    // 로그인 요청
-            List.of("POST", "/sign/join") // 회원가입 요청
-    );
-
-    // 인증이 필요한 경로인지 확인
     public boolean isSecured(ServerHttpRequest request) {
-        // 인증이 필요하지 않은 경로라면 false 반환
-        boolean isOpenApi = openApiEndpoints.stream().anyMatch(methodAndPath ->
-                request.getMethod().toString().equals(methodAndPath.get(0)) &&
-                        request.getURI().getPath().contains(methodAndPath.get(1))
-        );
-
-        if (isOpenApi) {
-            return false;
-        }
-
-        // 인증이 필요한 경로인지 확인
         return securedApiEndpoints.stream().anyMatch(methodAndPath ->
                 request.getMethod().toString().equals(methodAndPath.get(0)) &&
                         request.getURI().getPath().contains(methodAndPath.get(1))
