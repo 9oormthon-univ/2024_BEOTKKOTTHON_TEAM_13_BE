9012593abca4e22eb9a49a609bb5cc52ab3359a3
joamksh <joamksh@naver.com>
2025-01-21 20:30:49 +0900

    ref : 기존 request 로직 일부 converter로 처리

--- service-user/src/main/java/com/team13/serviceuser/controller/SignController.java
+++ service-user/src/main/java/com/team13/serviceuser/controller/SignController.java
@@ -2,12 +2,11 @@ package com.team13.serviceuser.controller;
 
 import com.team13.serviceuser.apiPyaload.ApiResponse;
 import com.team13.serviceuser.apiPyaload.code.status.ErrorStatus;
-import com.team13.serviceuser.dto.JoinRequestDto;
 import com.team13.serviceuser.dto.LoginRequestDto;
+import com.team13.serviceuser.dto.SignRequest;
 import com.team13.serviceuser.entity.User;
 import com.team13.serviceuser.service.SignInService;
 import com.team13.serviceuser.service.SignUpService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,9 +30,8 @@ public class SignController {
     private final SignUpService signUpService;
 
     //회원가입 요청
-    // 회원가입 요청
     @PostMapping("/join")
-    public ResponseEntity<ApiResponse<Object>> join(@Valid @RequestBody JoinRequestDto joinRequestDto,
+    public ResponseEntity<ApiResponse<Object>> join(@Valid @RequestBody SignRequest.RegisterRequestDto registerRequestDto,
                                                     BindingResult bindingResult) {
         // Request 전달 값 조건 에러시 메시지
         if (bindingResult.hasErrors()) {
@@ -47,26 +45,27 @@ public class SignController {
         }
 
         // 이메일 중복 체크
-        if (signUpService.checkEmailDuplicate(joinRequestDto.getEmail())) {
+        if (signUpService.checkEmailDuplicate(registerRequestDto.getEmail())) {
             return ResponseEntity.badRequest().body(
                     ApiResponse.onFailure(ErrorStatus.REGISTER4002.getCode(), ErrorStatus.REGISTER4002.getMessage(), null));
         }
 
         // 닉네임 중복 체크
-        if (signUpService.checkNicknameDuplicate(joinRequestDto.getNickname())) {
+        if (signUpService.checkNicknameDuplicate(registerRequestDto.getNickname())) {
             return ResponseEntity.badRequest().body(
                     ApiResponse.onFailure(ErrorStatus.REGISTER4003.getCode(), ErrorStatus.REGISTER4003.getMessage(), null));
         }
 
         // 비밀번호와 비밀번호 확인 일치 여부 체크
-        if (!joinRequestDto.getPassword().equals(joinRequestDto.getPasswordCheck())) {
+        if (!registerRequestDto.getPassword().equals(registerRequestDto.getPasswordCheck())) {
             return ResponseEntity.badRequest().body(
                     ApiResponse.onFailure(ErrorStatus.REGISTER4004.getCode(), ErrorStatus.REGISTER4004.getMessage(), null));
         }
 
-        signUpService.join(joinRequestDto);
+        signUpService.registerUser(registerRequestDto);
         return ResponseEntity.ok(ApiResponse.onSuccess("회원가입 성공"));
     }
+
     // 로그인 요청
     // 로그인 성공 시 클라이언트에게 JWT 토큰을 반환함
     @PostMapping("/login")
---
+++ service-user/src/main/java/com/team13/serviceuser/dto/SignRequest.java
@@ -0,0 +1,35 @@
+package com.team13.serviceuser.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class SignRequest {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class RegisterRequestDto{
+        @Email(message = "이메일은 유효해야합니다")
+        @NotBlank(message = "이메일을 입력하세요")
+        private String email;
+
+        @Size(min = 8, max = 16, message = "비밀번호는 최소 8자에서 16자여야 합니다.")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
+                message = "비밀번호는 각각 최소 1개의 대문자,소문자,숫자,특수기호를 포함해야합니다.")
+        @NotBlank(message = "비밀번호를 입력하세요")
+        private String password;
+
+        @NotBlank(message = "비밀번호 재확인 값을 입력하세요")
+        private String passwordCheck;
+
+        @NotBlank(message = "닉네임을 입력하세요")
+        private String nickname;
+    }
+}
