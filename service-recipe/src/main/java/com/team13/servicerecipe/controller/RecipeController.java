0908861879f7f4cca7865b5eb33a1a75871b2279
joamksh <joamksh@naver.com>
2025-01-21 12:43:54 +0900

    [레시피] (#75)
    * feat : 토큰에서 userId 추출 로직 추가
    
    * feat : 토큰값 userId 헤더로 받아서 사용자 정보 Get 수정
    
    * ref : mypage info정보 gateway 토큰 처리 적용
    
    * feat : 로그인, 회원가입 토큰 필요 없는 기능 따로 처리
    
    * ref : mypage API URL 변경
    
    * ref : gateway로 변경하며 필요 없어진 코드 삭제
    
    * ref : 로그인 토큰 필요 없는 경우 처리 수정
    
    * ref : 레시피 등록 userId gateway 토큰 추출 적용
    
    * ref : 레시피 단일 조회 gateway설정
    
    * ref : recipe에서 userId 필요한 경우 gateway필터 적용되게 수정
    feign연결 역시 header-userId 사용하도록 수정
    
    * ref : recipe관련 기능 gateway 적용
    
    * ref : 불필요한 확인 과정 삭제

--- service-recipe/src/main/java/com/team13/servicerecipe/controller/RecipeController.java
+++ service-recipe/src/main/java/com/team13/servicerecipe/controller/RecipeController.java
@@ -7,6 +7,7 @@ import com.team13.servicerecipe.dto.RecipeResponseDto;
 import com.team13.servicerecipe.entity.Recipe;
 import com.team13.servicerecipe.service.LikeRecipeService;
 import com.team13.servicerecipe.service.RecipeService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,25 +21,30 @@ import java.util.Optional;
 public class RecipeController {
     @Autowired
     private RecipeService recipeService;
+
     @Autowired
     private LikeRecipeService likeRecipeService;
 
     @PostMapping
-    public ResponseEntity<RecipeResponseDto> createRecipe(
-            @RequestBody RecipeRequestDto recipeRequestDto,
-            @RequestParam Long userId
-    ) {
+    public ResponseEntity<RecipeResponseDto> createRecipe(@RequestBody RecipeRequestDto recipeRequestDto, HttpServletRequest request) {
+        // Gateway에서 전달한 사용자 ID를 헤더에서 가져옴
+        String userIdHeader = request.getHeader("X-User-Id");
         try {
+            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             RecipeResponseDto savedRecipeResponse = recipeService.createRecipeWithDetails(recipeRequestDto, userId);
             return new ResponseEntity<>(savedRecipeResponse, HttpStatus.CREATED);
+        } catch (NumberFormatException e) {
+            // 헤더에서 가져온 사용자 ID가 숫자로 변환되지 않을 경우
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            // 비즈니스 로직에서 발생하는 기타 예외 처리
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<RecipeResponseDto> getRecipeById(@PathVariable("id") Long id) {
-        Optional<RecipeResponseDto> recipeWithDetails = recipeService.getRecipeWithUserDetails(id);
+    @GetMapping("/{recipeId}")
+    public ResponseEntity<RecipeResponseDto> getRecipeById(@PathVariable("recipeId") Long recipeId) {
+        Optional<RecipeResponseDto> recipeWithDetails = recipeService.getRecipeWithUserDetails(recipeId);
         if (recipeWithDetails.isPresent()) {
             return ResponseEntity.ok(recipeWithDetails.get());
         } else {
@@ -46,18 +52,25 @@ public class RecipeController {
         }
     }
 
-    // localhost:4328:1/like?userId=2
-    //userId가 2인 사람이 Recipe1번 글 좋아요를 누른다.
     @PostMapping("/{recipeId}/like")
-    public ResponseEntity<String> toggleLikeRecipe(@PathVariable("recipeId") Long recipeId, @RequestParam Long userId) {
-        boolean success = likeRecipeService.toggleLikeRecipe(recipeId, userId);
-        if (success) {
-            return ResponseEntity.ok("게시글에 대한 좋아요 혹은 좋아요 취소가 실행됐습니다");
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("해당 게시글 혹은 유저확인이 문제로 좋아요 관련 기능이 실행되지 않았습니다.");
+    public ResponseEntity<String> toggleLikeRecipe(@PathVariable("recipeId") Long recipeId, HttpServletRequest request) {
+        String userIdHeader = request.getHeader("X-User-Id");
+
+        if (userIdHeader == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+        try {
+            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
+            boolean success = likeRecipeService.toggleLikeRecipe(recipeId, userId);
+            if (success) {
+                return ResponseEntity.ok("게시글에 대한 좋아요 혹은 좋아요 취소가 실행됐습니다");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("해당 게시글 혹은 유저확인이 문제로 좋아요 관련 기능이 실행되지 않았습니다.");
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
-
 
     //단순 확인용 나중에 지울예정 postResponseDto에서 좋아요수까지 확인가능
     @GetMapping("/{recipeId}/likes")
@@ -66,23 +79,35 @@ public class RecipeController {
         return ResponseEntity.ok(likesCount);
     }
 
-    @GetMapping("/user/{userId}")
-    public ResponseEntity<List<MypageRecipeResponseDto>> getRecipesByUserId(@PathVariable("userId") Long userId) {
-        List<MypageRecipeResponseDto> recipes = recipeService.getRecipesByUserId(userId);
-        if (recipes.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } else {
-            return ResponseEntity.ok(recipes);
+    @GetMapping("/user")
+    public ResponseEntity<List<MypageRecipeResponseDto>> getRecipesByUserId(HttpServletRequest request) {
+        String userIdHeader = request.getHeader("X-User-Id");
+        try {
+            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
+            List<MypageRecipeResponseDto> recipes = recipeService.getRecipesByUserId(userId);
+            if (recipes.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            } else {
+                return ResponseEntity.ok(recipes);
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
-    @GetMapping("/like/user/{userId}")
-    public ResponseEntity<List<MypageRecipeResponseDto>> getLikeRecipesByUserId(@PathVariable("userId") Long userId) {
-        List<MypageRecipeResponseDto> likedRecipes = recipeService.getLikeRecipesByUserId(userId);
-        if (likedRecipes.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        } else {
-            return ResponseEntity.ok(likedRecipes);
+    @GetMapping("/like/user")
+    public ResponseEntity<List<MypageRecipeResponseDto>> getLikeRecipesByUserId(HttpServletRequest request) {
+        String userIdHeader = request.getHeader("X-User-Id");
+        try {
+            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
+            List<MypageRecipeResponseDto> likedRecipes = recipeService.getLikeRecipesByUserId(userId);
+            if (likedRecipes.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            } else {
+                return ResponseEntity.ok(likedRecipes);
+            }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
--- service-user/src/main/java/com/team13/serviceuser/feign/RecipeServiceClient.java
+++ service-user/src/main/java/com/team13/serviceuser/feign/RecipeServiceClient.java
@@ -4,15 +4,16 @@ import com.team13.serviceuser.dto.MypageRecipeResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
 @FeignClient(name = "service-recipe")
 public interface RecipeServiceClient {
 
-    @GetMapping("/recipes/user/{userId}")
-    List<MypageRecipeResponseDto> getRecipesByUserId(@PathVariable("userId") Long userId);
+    @GetMapping("/recipes/user")
+    List<MypageRecipeResponseDto> getRecipesByUserId(@RequestHeader("X-User-Id") Long userId);
 
-    @GetMapping("/recipes/like/user/{userId}")
-    List<MypageRecipeResponseDto> getLikeRecipesByUserId(@PathVariable("userId") Long userId);
+    @GetMapping("/recipes/like/user")
+    List<MypageRecipeResponseDto> getLikeRecipesByUserId(@RequestHeader("X-User-Id") Long userId);
 }
