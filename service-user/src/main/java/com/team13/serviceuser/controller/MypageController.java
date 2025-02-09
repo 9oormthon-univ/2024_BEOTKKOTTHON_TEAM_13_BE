42c8252b1c7076b58ccc8db15ec5253934e39660
joamksh <joamksh@naver.com>
2025-02-09 23:12:41 +0900

    ref : mypage @RequestHeader 적용

--- service-user/src/main/java/com/team13/serviceuser/controller/MypageController.java
+++ service-user/src/main/java/com/team13/serviceuser/controller/MypageController.java
@@ -26,11 +26,8 @@ public class MypageController {
 
     // 사용자 정보 가져오기
     @GetMapping("/info")
-    public ResponseEntity<UserDto> getUserInfo(HttpServletRequest request) {
-        // Gateway에서 전달한 사용자 ID를 헤더에서 가져옴
-        String userIdHeader = request.getHeader("X-User-Id");
+    public ResponseEntity<UserDto> getUserInfo(@RequestHeader("X-User-Id") Long userId) {
         try {
-            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             UserDto user = myPageService.getUserById(userId); // 서비스 호출
             return ResponseEntity.ok(user);
         } catch (NumberFormatException e) {
@@ -40,10 +37,8 @@ public class MypageController {
 
     // 사용자가 작성한 공동구매게시글 가져오기
     @GetMapping("/posts")
-    public ResponseEntity<List<MypagePostResponseDto>> getPostsByUserId(HttpServletRequest request) {
-        String userIdHeader = request.getHeader("X-User-Id");
+    public ResponseEntity<List<MypagePostResponseDto>> getPostsByUserId(@RequestHeader("X-User-Id") Long userId) {
         try {
-            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             List<MypagePostResponseDto> posts = myPageService.getPostsByUserId(userId);
             return ResponseEntity.ok(posts);
         } catch (NumberFormatException e) {
@@ -53,10 +48,8 @@ public class MypageController {
 
     //사용자가 작성한 레시피 가져오기
     @GetMapping("/recipes")
-    public ResponseEntity<List<MypageRecipeResponseDto>> getRecipesByUserId(HttpServletRequest request) {
-        String userIdHeader = request.getHeader("X-User-Id");
+    public ResponseEntity<List<MypageRecipeResponseDto>> getRecipesByUserId(@RequestHeader("X-User-Id") Long userId) {
         try {
-            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             List<MypageRecipeResponseDto> recipes = myPageService.getRecipesByUserId(userId);
             return ResponseEntity.ok(recipes);
         } catch (NumberFormatException e) {
@@ -66,10 +59,8 @@ public class MypageController {
 
     //사용자가 좋아요 누른 공동구매게시글
     @GetMapping("/likePosts")
-    public ResponseEntity<List<MypagePostResponseDto>> getLikePostsByUserId(HttpServletRequest request) {
-        String userIdHeader = request.getHeader("X-User-Id");
+    public ResponseEntity<List<MypagePostResponseDto>> getLikePostsByUserId(@RequestHeader("X-User-Id") Long userId) {
         try {
-            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             List<MypagePostResponseDto> likedPosts = myPageService.getLikePostsByUserId(userId);
             if (likedPosts.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -83,10 +74,8 @@ public class MypageController {
 
     //사용자가 좋아요 누른 레시피
     @GetMapping("/likeRecipes")
-    public ResponseEntity<List<MypageRecipeResponseDto>> getLikeRecipesByUserId(HttpServletRequest request) {
-        String userIdHeader = request.getHeader("X-User-Id");
+    public ResponseEntity<List<MypageRecipeResponseDto>> getLikeRecipesByUserId(@RequestHeader("X-User-Id") Long userId) {
         try {
-            Long userId = Long.valueOf(userIdHeader); // 사용자 ID 파싱
             List<MypageRecipeResponseDto> likedRecipes = myPageService.getLikeRecipesByUserId(userId);
             if (likedRecipes.isEmpty()) {
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
@@ -103,6 +92,4 @@ public class MypageController {
         UserDto updatedUser = myPageService.updateUser(userId, userDto);
         return ResponseEntity.ok(updatedUser);
     }
-
-
 }
