d935250d3ab7f5a953d2868128df5f014501b01c
joamksh <joamksh@naver.com>
2025-05-20 17:18:51 +0900

    ref : profile 업로드 수정

--- service-user/src/main/java/com/team13/serviceuser/service/SignUpService.java
+++ service-user/src/main/java/com/team13/serviceuser/service/SignUpService.java
@@ -7,11 +7,18 @@ import com.team13.serviceuser.feign.ChatServiceClient;
 import com.team13.serviceuser.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
 @Transactional
@@ -23,6 +30,12 @@ public class SignUpService {
     private final BCryptPasswordEncoder encoder;
 
     private final ChatServiceClient chatServiceClient;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Value("${file.default-profile}")
+    private String defaultProfilePath;
 
     public boolean checkEmailDuplicate(String email) {
         return userRepository.existsByEmail(email);
@@ -36,8 +49,21 @@ public class SignUpService {
     //비밀번호 암호화
 
     public void registerUser(SignRequest.RegisterRequestDto registerRequestDto) {
-        User savedUser = userRepository.save(SignConverter.toUserEntity(registerRequestDto, encoder.encode(registerRequestDto.getPassword())));
+        String fileName = UUID.randomUUID() + ".png";
+        String profileImageUrl = "/images/profile/" + fileName;
 
+        try {
+            File dest = new File(uploadDir + fileName);
+            if (!dest.getParentFile().exists()) dest.getParentFile().mkdirs();
+
+            Files.copy(Paths.get(defaultProfilePath), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException("기본 프로필 이미지 복사 실패", e);
+        }
+
+        User savedUser = userRepository.save(
+                SignConverter.toUserEntity(registerRequestDto, encoder.encode(registerRequestDto.getPassword()), profileImageUrl)
+        );
         // TOOD: 개발 단계에서 모든 유저는 테스트 채팅방에 초대함 (추후 테스트 완료시 아래 코드 제거)
         Map<String, String> requestBody = new HashMap<>();
         requestBody.put("chatroomId", "test-chatroom");
